04cf9a57bd240f313a440b41efb9a7c9b46f301b
LesediMarumo <lesedimarumo@gmail.com>
2023-07-24 12:26:42 -0700

    adding task 7

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(void) {
     size_t bufsize = 0;
     ssize_t characters;
     char *path_env = getenv("PATH");
-    char *path_token = strtok(path_env, ":");
+    char *path_token = _strtok(path_env, ":");
     char path_command[BUFFER_SIZE];
 
     while (1)
@@ -27,10 +27,10 @@ int main(void) {
         if (characters > 0 && command[characters - 1] == '\n')
             command[characters - 1] = '\0';
         if (strcmp(command, "exit") == 0)
-            break;
+            _exit(command);
         else if (strcmp(command, "env") == 0)
             print_environment();
-        if (command[0] != '\0')
+        else if (command[0] != '\0')
         {
             char *args[MAX_ARGS];
             
@@ -41,7 +41,7 @@ int main(void) {
                 while (path_token != NULL) {
                     snprintf(path_command, BUFFER_SIZE, "%s/%s", path_token, args[0]);
                     execve(path_command, args, environ);
-                    path_token = strtok(NULL, ":");
+                    path_token = _strtok(NULL, ":");
                 }
                 perror("execve");
                 exit(EXIT_FAILURE);
--- shell.h
+++ shell.h
@@ -6,13 +6,16 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdbool.h> 
 
 #define BUFFER_SIZE 1024
 #define MAX_ARGS 10
+#define INITIAL_BUFFER_SIZE 64
 
 extern char **environ;
 
 void tokenizeCommand(char *command, char **args);
 void print_environment();
-
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+char* _strtok(char* str, const char* delim);
 #endif
