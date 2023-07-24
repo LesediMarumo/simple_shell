8f22c8dc42ceb60364c6be54c70811d69ab88597
LesediMarumo <lesedimarumo@gmail.com>
2023-07-24 12:36:53 -0700

    modified main

--- main.c
+++ main.c
@@ -26,8 +26,7 @@ int main(void) {
         }
         if (characters > 0 && command[characters - 1] == '\n')
             command[characters - 1] = '\0';
-        if (strcmp(command, "exit") == 0)
-            _exit(command);
+        
         else if (strcmp(command, "env") == 0)
             print_environment();
         else if (command[0] != '\0')
